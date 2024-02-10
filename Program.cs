66ce6472a31d1b959286b4abed12c41458a797dd
student <student@sibupk.su>
2024-02-10 20:01:34 +0700

    Описание изменений

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите год:");
+        int year =
+        int.Parse(Console.ReadLine());
+        if(year % 4 == 0)
+        {
+            Console.WriteLine("{0} Год - Високосный", year);
+        }
+        else
+        {
+            Console.WriteLine("{0} Год - Не високосный", year);
+        }
+    }
+}
